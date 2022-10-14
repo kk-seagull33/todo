375f4b6780d2443b07ca0ad22ce3c6bc83a6ca21
kk-seagull33 <kk.seagull33@gmail.com>
2022-10-14 14:19:54 +0900

    add

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('todos', TodoItemController::class, ['only' => [
+        'index',
+        'store',
+        'show',
+        'edit',
+        'update',
+        'destroy',
+    ]]);
+    Route::patch('todos/{todo}/done', [TodoItemController::class, 'done']);
+    Route::patch('todos/{todo}/undone', [TodoItemController::class, 'undone']);
 });
